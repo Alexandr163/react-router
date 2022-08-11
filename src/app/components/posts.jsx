afe718fa8d7a5c3c5fa9d8b237a6ba50e99d53cf
Alexandr Morgunkov <shadow091@mail.ru>
2022-08-11 22:02:22 +0400

    finished homework

--- src/app/components/posts.jsx
+++ src/app/components/posts.jsx
@@ -1,13 +1,15 @@
 import PostsList from "./postsList";
 import Post from "./post";
+import { useParams } from "react-router-dom";
 
-const Posts = ({ match }) => {
+const Posts = () => {
+  const params = useParams();
   const posts = [
     { id: 1, label: "post 1" },
     { id: 2, label: "post 2" },
     { id: 3, label: "post 3" },
-  ];
-  const postId = match.params.postId;
+  ]; 
+  const { postId } = params;
 
   return (
     <>
